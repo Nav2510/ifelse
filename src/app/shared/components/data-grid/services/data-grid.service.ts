5e5681845b1120b34353ecb85587a7c728904721
nav2510 <singh.navdeep2510@gmail.com>
2024-03-26 17:10:39 +0530

    removed url and included mocks
    - removed private url
    - added mocks for data grid

--- src/app/shared/components/data-grid/services/data-grid.service.ts
+++ src/app/shared/components/data-grid/services/data-grid.service.ts
@@ -1,18 +1,15 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
-import { HttpClient } from '@angular/common/http';
 import { GridResponse } from '../models/grid-response.model';
+import { GRID_RESPONSE } from './grid-response.mocks';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataGridService {
-  private url = 'https://1.api.fy23ey04.careers.ifelsecloud.com/';
-
-  constructor(private readonly http: HttpClient) {}
 
   fetchData(): Observable<GridResponse> {
-    return this.http.get<GridResponse>(this.url);
+    return of(GRID_RESPONSE);
   }
 }
